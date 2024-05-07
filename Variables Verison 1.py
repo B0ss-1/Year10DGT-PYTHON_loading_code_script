87549af286f1c9ba3281ff49d8e3504852cc2797
B0ss-1 <168596551+B0ss-1@users.noreply.github.com>
2024-05-08 10:01:27 +1200

    Add files via upload

---
+++ Variables Verison 1.py
@@ -0,0 +1,48 @@
+# Variables
+# Author: Barnaby walker
+# Date: 3/05/24
+# Verson 1
+
+# Variables
+x=3
+y=8
+print(x+y)
+print("5"+"5") # Adding strings together is called concatenation
+sum="Hello "+"world!"
+print(sum)
+x=x+1
+y=y-2
+print(y+x)
+# We set up veriables using =
+string_eg1="This is a string" # this is self explanatory
+string_eg2="7"
+integer_eg=7 # no decimel point 
+float_eg=7.25 # decimel point 
+bool_eg1=True # True/False 
+bool_eg2=False 
+#create loop
+loadingloop="y"
+while loadingloop=="y":
+    print("Loading.")
+    print("Loading..")
+    print("Loading...")
+    print("Loading.")
+    print("Loading..")
+    print("Loading...") 
+    print("Loading.")
+    print("Loading..")
+    print("Loading...")
+    print("Loading.")
+    print("Loading..")
+    print("Loading...")
+    print("Loading.")
+    print("Loading..")
+    print("Loading...") 
+    print("Loading.")
+    print("Loading..")
+    print("Loading...")
+    loadingloop=input ("retry? ")
+    if loadingloop=="Y":
+        loadingloop="y"
+
+
